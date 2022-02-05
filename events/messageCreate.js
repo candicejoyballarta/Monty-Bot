e5bb6959b16d2e2aea9ec1590a2bd820b31e64d0
Candice Ballarta <candicejoy.ballarta@tup.edu.ph>
2022-02-06 03:20:46 +0800

    Command and Event Handler

---
+++ events/messageCreate.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'messageCreate',
+	async execute(message) {
+		// if (!message.content.startsWith(prefix) || message.author.bot) return;
+		// const args = message.content.slice(prefix.length).split(/ + /);
+		// const command = args.shift().toLowerCase();
+		// if (command === 'clear') {
+		// 	message.client.commands.get('clear').execute(message, args);
+		// }
+	},
+};
---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const fs = require('fs');
+const { Client, Intents, Collection } = require('discord.js');
+
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+const commandFiles = fs
+	.readdirSync('./commands')
+	.filter((file) => file.endsWith('.js'));
+
+const commands = [];
+
+client.commands = new Collection();
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// console.log(command.data);
+	commands.push(command.data.toJSON());
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs
+	.readdirSync('./events')
+	.filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, commands));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, commands));
+	}
+}
+
+client.login(process.env.TOKEN);
